bb82bc6e8bfccb4f9541eda4d3387a89d5c6239b
Tomohiro Kumagai <tomohiro@ez-style.jp>
2017-12-18 03:44:03 +0900

    Separated targets into iOS and macOS.

---
+++ Swim_iOS/Swim_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Swim_iOS.h
+//  Swim_iOS
+//
+//  Created by Tomohiro Kumagai on 12/18/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swim_iOS.
+FOUNDATION_EXPORT double Swim_iOSVersionNumber;
+
+//! Project version string for Swim_iOS.
+FOUNDATION_EXPORT const unsigned char Swim_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swim_iOS/PublicHeader.h>
+
+
---
+++ Swim_macOS/Swim_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Swim_macOS.h
+//  Swim_macOS
+//
+//  Created by Tomohiro Kumagai on 12/18/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swim_macOS.
+FOUNDATION_EXPORT double Swim_macOSVersionNumber;
+
+//! Project version string for Swim_macOS.
+FOUNDATION_EXPORT const unsigned char Swim_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swim_macOS/PublicHeader.h>
+
+

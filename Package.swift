e0a81a11ab8cbe85e19ec01f6ba900b93bcdc1af
Tomohiro Kumagai <tomohiro@ez-style.jp>
2019-09-27 23:29:17 +0900

    Update for SwiftPM.

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Swim",
+    platforms: [
+        .macOS(.v10_14)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Swim",
             targets: ["Swim"]),
@@ -24,5 +26,6 @@ let package = Package(
         .testTarget(
             name: "SwimTests",
             dependencies: ["Swim"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )

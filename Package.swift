0676ca1a69d4958ddae8d29881c696a241b9bf9a
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-01-12 11:37:39 +0900

    Modify Package.swift.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Swim",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -24,5 +27,7 @@ let package = Package(
         .testTarget(
             name: "SwimTests",
             dependencies: ["Swim"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
+
 )

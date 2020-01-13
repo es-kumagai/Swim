eb53749e77a8ed3994b2069a33374dc95b457a7e
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-01-13 13:43:58 +0900

    Add `ProcessExitStatus` which express a process exit status.

---
+++ Sources/Swim/State/ProcessExitStatus.swift
@@ -0,0 +1,61 @@
+//
+//  State.swift
+//  Swim
+//
+//  Created by Tomohiro Kumagai on 2020/01/13.
+//
+
+
+@available(*, unavailable, renamed: "ProcessExitStatus")
+public enum ProcessingState {}
+
+/// Expressing Process Exit Status
+public struct ProcessExitStatus {
+    
+    var code: Int
+
+    public init<T: BinaryInteger>(code: T) {
+
+        self.code = Int(code)
+    }
+}
+
+extension ProcessExitStatus : ExpressibleByIntegerLiteral {
+
+    public init(integerLiteral value: Int) {
+        
+        self.init(code: value)
+    }
+}
+
+extension ProcessExitStatus {
+    
+    /**
+    Returns a Boolean value that indicates whether the value is equal to .Passed.
+    */
+    public var passed: Bool {
+        
+        code.meansProcessPassed
+    }
+    
+    /**
+    Returns a Boolean value that indicates whether the value is equal to .Aborted.
+    */
+    public var aborted: Bool {
+
+        code.meansProcessAborted
+    }
+}
+
+extension Numeric {
+    
+    public var meansProcessPassed: Bool {
+        
+        self == 0
+    }
+    
+    public var meansProcessAborted: Bool {
+        
+        self != 0
+    }
+}
---
+++ Tests/SwimTests/StateTests.swift
@@ -0,0 +1,48 @@
+//
+//  StateTests.swift
+//  SwimTests
+//
+//  Created by Tomohiro Kumagai on 2020/01/13.
+//
+
+import XCTest
+@testable import Swim
+
+class StateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testProcessExitStatus() {
+
+        let pass = 0 as ProcessExitStatus
+        let abort1 = 1 as ProcessExitStatus
+        let abort2 = ProcessExitStatus(code: 25)
+        let abort3 = -1 as ProcessExitStatus
+        
+        XCTAssertTrue(pass.passed)
+        XCTAssertFalse(pass.aborted)
+        XCTAssertFalse(abort1.passed)
+        XCTAssertTrue(abort1.aborted)
+        XCTAssertFalse(abort2.passed)
+        XCTAssertTrue(abort2.aborted)
+        XCTAssertFalse(abort3.passed)
+        XCTAssertTrue(abort3.aborted)
+        
+        XCTAssertTrue(0.meansProcessPassed)
+        XCTAssertFalse(0.meansProcessAborted)
+        XCTAssertFalse(1.meansProcessPassed)
+        XCTAssertTrue(1.meansProcessAborted)
+        
+        XCTAssertEqual(abort2.code, 25)
+    }
+
+    static var allTests = [
+        ("testProcessExitStatus", testProcessExitStatus),
+    ]
+}

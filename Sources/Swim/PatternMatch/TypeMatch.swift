abea74eec4ab23ee3effd2a3fe384e70e98e9247
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-12-08 18:14:07 +0900

    Add a pattern match operator that determines if a value matches a pattern.

---
+++ Sources/Swim/PatternMatch/TypeMatch.swift
@@ -0,0 +1,16 @@
+//
+//  TypeMatch.swift
+//  Swim
+//
+//  Created by Tomohiro Kumagai on 2020/12/08.
+//
+
+/// [Swim] Returns a Boolean value indicating whether both `pattern` and `value` is the same type.
+/// - Parameters:
+///   - pattern: A metatype.
+///   - value: A value to match against `pattern`.
+/// - Returns: True iff the type that passed by `value` is equal to `pattern`, otherwise false.
+public func ~= (pattern: Any.Type, value: Any.Type) -> Bool {
+
+    return pattern == value
+}

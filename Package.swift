d0d225dc30b68cfc2eb4b46236a04f59968a518e
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-01-10 13:50:53 +0900

    iOS has been supported.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Swim",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.

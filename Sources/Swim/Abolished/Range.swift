dc1e49d3fa58d0fc399dc88200dee1e2219cba97
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-02-14 16:32:55 +0900

    `PartialRange` has been abolished.
    This feature was being introduced on an experimental basis.

---
+++ Sources/Swim/Abolished/Range.swift
@@ -0,0 +1,10 @@
+//
+//  Range.swift
+//  Swim
+//
+//  Created by Tomohiro Kumagai on 12/14/17.
+//
+
+/// EXPERIMENTAL
+@available(*, unavailable, message: "This experimental feature has been abolished.")
+public struct PartialRange<Bound> where Bound : Comparable {}

065cb3dfb8baa54e86b1e80d4dbfa83c68e92c79
Tomohiro Kumagai <tomo@ez-net.jp>
2024-08-22 01:48:47 +0900

    Enhance switch statement.

--- Sources/Swim/PatternMatch/FunctionMatch.swift
+++ Sources/Swim/PatternMatch/FunctionMatch.swift
@@ -5,12 +5,22 @@
 //  Created by Tomohiro Kumagai on 2021/02/17.
 //
 
-/// [Swim] Returns a Boolean value that returned by `function` given `value`.
+/// [Swim] Returns a Boolean value that returned by `functionPattern` with `value`.
 /// - Parameters:
-///   - pattern: A function that applying to `value`.
+///   - functionPattern: A function that applying to `value`.
 ///   - value: A value that is given to `function`.
-/// - Returns: The return value by `function` with `value`.
-@inlinable public func ~= <T>(pattern: (T) -> Bool, value: T) -> Bool {
+/// - Returns: A value returned by calling `function` with `value`.
+@inlinable
+public func ~= <Value>(functionPattern: (Value) -> Bool, value: Value) -> Bool {
+    functionPattern(value)
+}
 
-    return pattern(value)
+/// [Swim] Returns a Boolean value that returned by a value accessing by `keyPathPattern` by `target`.
+/// - Parameters:
+///   - keyPathPattern: A key path to accessing a property of the `target`.
+///   - target: An instance that will be accessed.
+/// - Returns: A value returned by accessing the property using `functionPattern` of the `target`.
+@inlinable
+public func ~= <Target>(keyPathPattern: KeyPath<Target, Bool>, target: Target) -> Bool {
+    target[keyPath: keyPathPattern]
 }

d0ac577c3186e1359b65e4dac1bcc4eafecc28b3
Tomohiro Kumagai <tomo@ez-net.jp>
2024-09-07 12:28:18 +0900

    Generalize RawRepresentable.

---
+++ Sources/Swim/String/GeneralizedRawRepresentable.swift
@@ -0,0 +1,38 @@
+//
+//  GeneralizedRawRepresentable.swift
+//
+//  
+//  Created by Tomohiro Kumagai on 2024/08/22
+//  
+//
+
+public extension RawRepresentable where RawValue == String {
+    
+    init(rawValue: some StringProtocol) {
+        self.init(rawValue: String(rawValue))!
+    }
+}
+
+public extension RawRepresentable where RawValue: BinaryInteger {
+    
+    init?(exactly rawValue: some BinaryInteger) {
+
+        guard let rawValue = RawValue(exactly: rawValue) else {
+            return nil
+        }
+
+        self.init(rawValue: rawValue)
+    }
+}
+
+public extension RawRepresentable where RawValue: BinaryFloatingPoint {
+    
+    init?(exactly rawValue: some BinaryFloatingPoint) {
+        
+        guard let rawValue = RawValue(exactly: rawValue) else {
+            return nil
+        }
+        
+        self.init(rawValue: rawValue)
+    }
+}

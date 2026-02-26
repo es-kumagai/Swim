5e2bc768cbada352dfef6edd5fd73ec4924aae9b
Tomohiro Kumagai <tomo@ez-net.jp>
2026-02-26 12:13:09 +0900

    Redesign UnsafeSendingObject.

--- Sources/Swim/Concurrency/UnsafeSendingObject.swift
+++ Sources/Swim/Concurrency/UnsafeSendingObject.swift
@@ -7,6 +7,11 @@
 
 /// [Swim] A wrapper that allows an object to cross an isolation boundary
 /// without being blocked by concurrency safety checks.
+@available(*, unavailable, renamed: "UnsafeSending")
+public struct UnsafeSendingObject: Sendable {}
+
+/// [Swim] A wrapper that allows an instance to cross an isolation boundary
+/// without being blocked by concurrency safety checks.
 ///
 /// Use this when you intentionally pass a value across an isolation
 /// boundary even though it does not conform to `Sendable` despite being
@@ -15,10 +20,42 @@
 ///
 /// The programmer is responsible for guaranteeing that it is actually
 /// safe to use across the isolation boundary.
-public struct UnsafeSendingObject<Wrapped: AnyObject>: @unchecked Sendable where Wrapped: SendableMetatype {
+public struct UnsafeSending<Wrapped>: @unchecked Sendable where Wrapped: SendableMetatype {
     public let value: Wrapped
     
     public init(_ value: consuming Wrapped) {
         self.value = value
+    }
+}
+
+extension UnsafeSending: CustomStringConvertible where Wrapped: CustomStringConvertible {
+    
+    public var description: String {
+        value.description
+    }
+}
+
+extension UnsafeSending: CustomDebugStringConvertible where Wrapped: CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        "Unsafe Sending: \(value.debugDescription)"
+    }
+}
+
+extension UnsafeSending: Equatable where Wrapped: Equatable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.value == rhs.value
+    }
+}
+
+extension UnsafeSending: Hashable where Wrapped: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        value.hash(into: &hasher)
+    }
+}
+
+extension UnsafeSending: Comparable where Wrapped: Comparable {
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        lhs.value < rhs.value
     }
 }
